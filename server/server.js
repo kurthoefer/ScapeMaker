47eb6a74177301443e2e80126e7ba7a68f805928
kurthoefer <kurthoefer@KURTs-MacBook-Pro.local>
2017-03-08 20:36:07 -0800

    deleting / organizing comments

--- server/server.js
+++ server/server.js
@@ -29,24 +29,8 @@ db.once('open', function() {
 	console.log('we are connected to the db')
 })
 
-
+//seed db with dummy values
 dbSeeder();
-// let seedScapes = [
-// 	{
-// 		title: 'FILL1',
-// 		skyUrl: 'https://pbs.twimg.com/profile_images/2968304317/652317f26fb6506d89938d1104bd68b1.jpeg',
-// 		landUrl: 'https://pbs.twimg.com/profile_images/2968304317/652317f26fb6506d89938d1104bd68b1.jpeg'
-// 	},
-// 	{
-// 		title: 'FILL2',
-// 		skyUrl: 'https://pbs.twimg.com/profile_images/2968304317/652317f26fb6506d89938d1104bd68b1.jpeg',
-// 		landUrl: 'https://pbs.twimg.com/profile_images/2968304317/652317f26fb6506d89938d1104bd68b1.jpeg'
-// 	}
-// ]
-
-// // drop and rebuild my db with entries from 'seedScapes'
-// db.collections['scapes'].drop()
-// for (let entry of seedScapes) Scape.create(entry);
 
 
 app.use(express.static(__dirname + '/../client'));
